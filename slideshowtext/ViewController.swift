d808d7f562579fdfc1ac0adcfefbe80355bc7074
humayun kabir <humayun_kabir@appnap.io>
2019-10-26 11:21:08 +0600

    Initial Commit

---
+++ slideshowtext/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  slideshowtext
+//
+//  Created by Appnap WS02 on 26/10/19.
+//  Copyright © 2019 Appnap WS02. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
